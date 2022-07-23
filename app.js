696a6b6768b50859f8f6a2e9d4bbbb2f85c200aa
Neil Martin <neilmartindotdev@protonmail.com>
2022-07-23 15:49:10 +0100

    async and await

--- app.js
+++ app.js
@@ -31,4 +31,18 @@ function loadGIF(e) {
     });
 }
 
+/* Refactored code using async and await
+const refactorimg = document.querySelector("img");
+
+async function getCats() {
+  const response = await fetch(
+    "https://api.giphy.com/v1/gifs/translate?api_key=YOUR_KEY_HERE&s=cats",
+    { mode: "cors" }
+  );
+  const catData = await response.json();
+  img.src = catData.data.images.original.url;
+}
+getCats();
+
 document.addEventListener("DOMContentLoaded", loadGIF);
+*/
